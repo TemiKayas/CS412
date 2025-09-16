0d2218064415252eba4b5b7540712f68b4c96991
TemiKayas <artemioskayas@gmail.com>
2025-09-16 11:43:01 -0400

    Added the really basic views functions to allow me to run server

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,19 @@
+from re import I
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
+import random
 # Create your views here.
+def main(request):
+    response_text = "This is the main page"
+    
+    return HttpResponse(response_text)
+
+def order(request):
+    response_text = "This is the order page"
+    
+    return HttpResponse(response_text)
+
+def confirmation(request):
+    response_text = "This is the confirmation page"
+    
+    return HttpResponse(response_text)
