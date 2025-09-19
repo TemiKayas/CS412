1a4adc6cafb7bfc13274c6354f7fe971ab7e9e5e
TemiKayas <artemioskayas@gmail.com>
2025-09-19 19:46:17 -0400

    order form

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from re import I
 from django.shortcuts import redirect, render
 from django.http import HttpRequest, HttpResponse
+import random
 # Create your views here.
 
 def main(request):
@@ -8,8 +9,21 @@ def main(request):
     return render(request, template_name)
 
 def order(request):
+    
+    specials_list = [
+        "Tzatziki sauce with extra garlic",
+        "Fried potato balls with cheese and bacon",
+        "Lemon and rice chicken soup",
+        "Stuffed peppers with rice and beef",
+        "Greek salad with feta cheese",
+        "Cheesy fries with bacon",
+    ]
+    
+    context = {
+        "daily_special": random.choice(specials_list)
+    }
     template_name = "restaurant/order.html"
-    return render(request, template_name)
+    return render(request, template_name, context)
 
 def confirmation(request):
     template_name = "restaurant/confirmation.html"    
