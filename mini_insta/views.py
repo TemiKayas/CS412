1bea098f6abebfb43e96f444a2846b01326282f9
TemiKayas <artemioskayas@gmail.com>
2025-09-25 12:02:57 -0400

    Added a show_all page for the home page and a simple for loop to iterate over the profiles.

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile
 
-# Create your views here.
+class ShowAllView(ListView):
+    '''View for the show all page'''
+    model = Profile
+    template_name = 'mini_insta/show_all.html'
+    context_object_name = 'profiles'
