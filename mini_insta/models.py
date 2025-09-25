8e0266e907c79a4fb7023a4c044621e3678c0320
TemiKayas <artemioskayas@gmail.com>
2025-09-25 11:51:29 -0400

    Added database stuff for profiles, and made it so you can see the username and display name on the django admin panel

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model): 
+    '''Data structure for the profile model '''
+
+    username = models.TextField(blank=True) 
+    display_name = models.TextField(blank=True)
+    profile_image_url = models.TextField(blank=True)
+    bio_text = models.TextField(blank=True)
+    join_date = models.DateField(auto_now=True)
+
+    def __str__(self): 
+        return f'{self.username} aka {self.display_name}'
