ad9312956787922529a8c3628e5a5aa8ef56f440
TemiKayas <artemioskayas@gmail.com>
2025-09-26 16:37:48 -0400

    Single profile page done, just have to add comments and stuff now

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from . import views
-from .views import show_all_profiles
+from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
-    path('', show_all_profiles.as_view(), name='show_all_profiles'),
+    path('', ProfileListView.as_view(), name='show_all_profiles'),
+    path('<int:pk>/', ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
-class show_all_profiles(ListView):
+class ProfileListView(ListView):
     '''View for the show all page'''
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+    '''View for the profile detail page'''
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
