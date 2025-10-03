a70d34215fbf70649c8e6215b94fe136818e1f72
TemiKayas <artemioskayas@gmail.com>
2025-10-03 14:42:45 -0400

    Finished project I do believe

--- mini_insta/forms.py
+++ mini_insta/forms.py
@@ -2,7 +2,7 @@
 # Author: Artemios Kayas (akayas@bu.edu) #
 # Description: Forms page to hold all my forms for the mini_insta app
 from django import forms
-from .models import Profile
+from .models import *
 
 class CreateProfileForm(forms.ModelForm):
     '''Form for the profile model'''
@@ -11,3 +11,18 @@ class CreateProfileForm(forms.ModelForm):
         '''Meta class for the profile form'''
         model = Profile
         fields = ['display_name', 'username', 'profile_image_url', 'bio_text']
+
+class CreatePostForm(forms.ModelForm):
+    '''Form for the post model'''
+    
+    image_url = forms.CharField(
+        required=False,
+        label='Image URL',
+        help_text='Enter the URL of an image for this post',
+        widget=forms.TextInput(attrs={'placeholder': 'https://example.com/image.jpg'})
+    )
+
+    class Meta:
+        '''Meta class for the post form'''
+        model = Post
+        fields = ['caption']  
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,10 +1,11 @@
-# File: views.py #}
-# Author: Artemios Kayas (akayas@bu.edu) #
+# File: views.py 
+# Author: Artemios Kayas (akayas@bu.edu) 
 # Description: Views page to display all my templates and holds logic to display pages
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
-from .models import Profile, Post
-from .forms import CreateProfileForm
+from django.urls import reverse
+from .models import Profile, Post, Photo
+from .forms import CreateProfileForm, CreatePostForm
 
 class ProfileListView(ListView):
     '''View for the show all page'''
@@ -30,3 +31,44 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'mini_insta/show_post.html'
     context_object_name = 'post'
+
+class CreatePostView(CreateView):
+    '''View for creating a new post'''
+    model = Post
+    form_class = CreatePostForm
+    template_name = 'mini_insta/create_post_form.html'
+    
+    def get_context_data(self, **kwargs):
+        '''Add the profile to the context data'''
+        context = super().get_context_data(**kwargs)
+        # get the profile from the URL parameter
+        profile_pk = self.kwargs['pk']
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        context['profile'] = profile
+        return context
+    
+    def form_valid(self, form):
+        '''Handle form submission - attach profile and create photo'''
+        # get the profile from the URL parameter
+        profile_pk = self.kwargs['pk']
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        
+        # attach the profile to the post before saving
+        form.instance.profile = profile
+        
+        # save the post 
+        response = super().form_valid(form)
+        
+        # create a photo object if image url was provided
+        image_url = form.cleaned_data.get('image_url')
+        if image_url:
+            Photo.objects.create(
+                post=self.object,  
+                image_url=image_url
+            )
+        
+        return response
+    
+    def get_success_url(self):
+        '''Redirect to the newly created post detail page'''
+        return reverse('show_post', kwargs={'pk': self.object.pk})
